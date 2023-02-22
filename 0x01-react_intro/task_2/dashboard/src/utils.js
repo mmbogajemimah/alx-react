613ca5d76b593c31044fba3271fa6d563db28f41
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-02-22 23:16:02 +0300

    Modify the App

---
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -0,0 +1,21 @@
+//import React from 'react';
+
+// export const getFullYear = () => {
+//   return new Date().getFullYear();
+// };
+
+// export const getFooterCopy = (isIndex) => {
+//   return `© ${getFullYear()} Holberton School main dashboard`;
+// };
+
+export function getFullYear() {
+  return new Date().getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+  if (isIndex) {
+    return 'Holberton School';
+  } else {
+    return 'Holberton School main dashboard';
+  }
+}
