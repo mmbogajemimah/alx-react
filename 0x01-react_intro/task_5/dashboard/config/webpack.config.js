10877c4daa251975b85d9964294014e3e19962e0
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-02-28 16:10:53 +0300

    Install Babel

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -21,6 +21,11 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
             },
