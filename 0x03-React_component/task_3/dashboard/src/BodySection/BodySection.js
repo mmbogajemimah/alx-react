118565f16c7982d1bc4d316080d818d349304daf
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-03-08 00:44:53 +0300

    Reusable comments & specialization

---
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySection.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class BodySection extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <>
+                <div className="bodySection">
+                    <h2>{this.props.title}</h2>
+                    {this.props.children}
+                </div>
+            </>
+        )
+    }
+}
+
+export default BodySection;
