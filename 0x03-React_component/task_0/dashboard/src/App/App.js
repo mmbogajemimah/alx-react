cad8724f5cb5f40327d663d8282f8f19308d8a17
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-03-07 13:59:19 +0300

    Commence with class components

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 //import logo from '../assets/holberton_logo.jpg';
 //import { getFooterCopy, getFullYear } from '../utils/utils';
@@ -10,37 +10,39 @@ import PropTypes from 'prop-types';
 import CourseList from '../CourseList/CourseList';
 import { getLatestNotification } from '../utils/utils';
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      isLoggedIn: props.isLoggedIn,
+      isLoggedIn: false,
       listCourses: [
-        {id: 1, name: 'ES6', credit: 60},
-        {id: 2, name: 'Webpack', credit: 20},
+        {id:1, name:'ES6', credit: 60},
+        {id:2, name:'Webpack', credit: 20},
         {id: 3, name: 'React', credit: 40}
       ],
       listNotifications: [
         {id: 1, value: "New course available", type: "default"},
         {id: 2, value: "New resume available", type: "urgent"},
         {id: 3, html: {__html: getLatestNotification()}, type: "urgent"},
-      ],
+      ]
     };
   }
 
   render() {
-    const { listCourses, listNotifications, isLoggedIn } = this.state;
-
+    // Destructuring in React
+    // Eg Before destructuring: <Notifications listNotifications={this.state.listNotifications}/>
+    const {listNotifications, isLoggedIn, listCourses} = this.state;
     return(
       <>
-        <Notifications listNotifications={listNotifications} />
-        <div className="App">
+        <Notifications listNotifications={listNotifications}/>
+        <div className='App'>
           <Header />
           {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
           <Footer />
         </div>
       </>
-    );
+    )
   }
 }
 
