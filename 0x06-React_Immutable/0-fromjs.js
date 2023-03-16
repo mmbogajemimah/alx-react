81ebdd3a6a9523e6c72c93144281b315f328edc0
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-03-16 15:12:36 +0300

    Converting into an Immutable object using fromJS

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,17 +1,17 @@
 // Converts palin JS objects and arrays to immutable Maps and Lists
-
 //Import formjs
 const { fromJS } = require('immutable')
 
-// let obj1 = {
-//     fear: true,
-//     smell: -1033575916.9145899,
-//     wall: false,
-//     thing: -914767132
-// }
-
-function getImmutableObject(obj) {
-    return (fromJS(obj));
+let obj1 = {
+    fear: true,
+    smell: -1033575916.9145899,
+    wall: false,
+    thing: -914767132
 }
 
-//getImmutableObject(obj1);
+function getImmutableObject(object) {
+    const obj = fromJS(object)
+    console.log(obj);
+}
+
+getImmutableObject(obj1);
