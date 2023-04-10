6c97aacc9af843dfee57abf4029ffdad761eb23e
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-04-10 12:39:21 +0300

    App.test.js

--- 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.test.js
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.test.js
@@ -12,6 +12,47 @@ import { shallow, mount } from "enzyme";
 import { StyleSheetTestUtils } from "aphrodite";
 import { AppContext, user, logOut } from "./AppContext";
 import { fromJS } from "immutable";
+import { createStore } from "redux";
+import uiReducer from "../reducers/uiReducer";
+
+describe('App Test', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+    const store = createStore(uiReducer);
+    wrapper = mount(
+      <Provider store={store}>
+        <App />
+      </Provider>)
+  });
+
+  it('Renders App Component without crashing', () => {
+    expect(wrapper.exists());
+  });
+
+  it('App component contains the Notification Component', () => {
+    expect(wrapper.find("Notifications")).toHaveLength(1);
+  });
+
+  it('App component contains the Header Component', () => {
+    expect(wrapper.find("Header")).toHaveLength(1);
+  });
+
+  it('App component contains the Login Component', () => {
+    expect(wrapper.find("Login")).toHaveLength(1);
+  });
+
+  it('App component contains the Footer Component', () => {
+    expect(wrapper.find("Footer")).toHaveLength(1);
+  });
+
+  it('test to check that CourseList is not displayed inside App', () => {
+    expect(wrapper.find("CourseList")).toHaveLength(0);
+  });
+
+});
+
 
 describe("<App />", () => {
   it("mapStateToProps returns the right object when the user Login", () => {
