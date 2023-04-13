fbe0f22a495e819ce0b15233c5935d2f3ebfa803
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-04-13 23:54:10 +0300

    Connect Logout action creator to the Header

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Header/Header.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Header/Header.js
@@ -1,14 +1,24 @@
 import React from "react";
 import logo from "../assets/holberton-logo.jpg";
 import { StyleSheet, css } from "aphrodite";
+import { connect } from 'react-redux';
+import * as uiActionCreators from '../actions/uiActionCreators'
 
-function Header() {
+function Header(props) {
+  const {user, logout} = props;
   return (
     <>
       <div className={css(styles["App-header"])}>
         <img src={logo} className={css(styles.img)} alt="logo" />
         <h1>School dashboard</h1>
       </div>
+      {
+        (user && (typeof user === 'object' ? Object.keys(user).length !== 0: false))
+        &&
+        <section id="logoutSection">
+          <h2>Welcome<strong>{user.email}</strong><em><a href="#" onClick={logout}>(logout)</a></em></h2>
+        </section>
+      }
     </>
   );
 }
@@ -28,4 +38,14 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Header;
+export function mapStateToProps(state) {
+  return {
+    user: state.get('user')
+  }
+}
+
+export const mapDispatchToProps = {
+  logout: uiActionCreators.logout,
+}
+
+export default connect(mapStateToProps)(Header);
