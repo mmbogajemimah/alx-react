39f439102719137bb7584f04ba30ed02fde51a67
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-04-14 00:48:46 +0300

    Connect notifications to the reducer

---
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/reducers/uiReducer.js
@@ -0,0 +1,44 @@
+/* we first import the Map object from Immutable.js and use it to create the 
+initial state. We also create a Map object for the user property, although it's 
+currently an empty object.
+
+Inside the reducer function, we use the set method of the Map object to update 
+the corresponding properties of the state. This returns a new Map object with 
+the updated property, while leaving the rest of the state unchanged.
+*/
+import { Map } from 'immutable';
+import * as actions from '../actions/uiActionTypes';
+
+
+const initialState = Map({
+    isNotificationDrawerVisible: false,
+    isUserLoggedIn: false,
+    user: Map({})
+});
+
+export default function uiReducer(state = initialState, action) {
+    switch(action.type) {
+        case actions.DISPLAY_NOTIFICATION_DRAWER:
+            return state.set('isNotificationDrawerVisible', true)
+        
+        case actions.HIDE_NOTIFICATION_DRAWER:
+            return state.set('isNotificationDrawerVisible', false)
+        
+        case actions.LOGIN_SUCCESS:
+            return state.set('isUserLoggedIn', true)
+        
+        case actions.LOGIN_FAILURE:
+            return state.set('isUserLoggedIn', false)
+
+        case actions.LOGIN:
+            return state.set("user", action.user);
+        case actions.LOGOUT:
+            return state.merge({
+                isUserLoggedIn: false,
+                user: null
+            });
+        
+        default:
+            return state
+    }
+}
